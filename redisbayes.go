2db1a6bdf3bdba3eccf4bdc31cc50d8128c5b9de
Pengfei Xue <pengphy@gmail.com>
2013-05-11 15:36:43 +0800

    use gofmt to reformate code style

--- redisbayes.go
+++ redisbayes.go
@@ -2,11 +2,11 @@ package redisbayes
 
 import (
 	//"math"
-    "regexp"
-    "log"
-    "strings"
-    "github.com/kylelemons/go-gypsy/yaml"
-    //"github.com/garyburd/redigo/redis"
+	"github.com/kylelemons/go-gypsy/yaml"
+	"log"
+	"regexp"
+	"strings"
+	//"github.com/garyburd/redigo/redis"
 )
 
 var english_ignore_words_map = make(map[string]int)
@@ -14,57 +14,57 @@ var english_ignore_words_map = make(map[string]int)
 // replace \_.,<>:;~+|\[\]?`"!@#$%^&*()\s chars with whitespace
 // re.sub(r'[\_.,<>:;~+|\[\]?`"!@#$%^&*()\s]', ' ' 
 func Tidy(s string) string {
-        reg, err := regexp.Compile("[\\_.,:;~+|\\[\\]?`\"!@#$%^&*()\\s]+")
-        if err != nil {
-                log.Fatal(err)
-        }
+	reg, err := regexp.Compile("[\\_.,:;~+|\\[\\]?`\"!@#$%^&*()\\s]+")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-        text_in_lower := strings.ToLower(s)
-        safe := reg.ReplaceAllLiteralString(text_in_lower, " ")
+	text_in_lower := strings.ToLower(s)
+	safe := reg.ReplaceAllLiteralString(text_in_lower, " ")
 
-        return safe
+	return safe
 }
 
 // tidy the input text, ignore those text composed with less than 2 chars 
 func English_tokenizer(s string) []string {
-        words := strings.Fields(Tidy(s))
+	words := strings.Fields(Tidy(s))
 
-        for index, word := range words {
-                strings.TrimSpace(word)
-                _, omit := english_ignore_words_map[word]
-                if omit || len(word)<2 {
-                        words = words[:index+copy(words[index:], words[index+1:])]
-                }
-        }
+	for index, word := range words {
+		strings.TrimSpace(word)
+		_, omit := english_ignore_words_map[word]
+		if omit || len(word) < 2 {
+			words = words[:index+copy(words[index:], words[index+1:])]
+		}
+	}
 
-        return words
+	return words
 }
 
 // compute word occurances
 func Occurances(w string) map[string]uint {
-        return nil
+	return nil
 }
 
 // init function, load the configs
 // fill english_ignore_words_map
 func init() {
-    // load config file
-    cfg_filename := "config.yaml"
-    config, err := yaml.ReadFile(cfg_filename)
-    if err != nil {
-            log.Fatalf("readfile(%s): %s", cfg_filename, err)
-    }
+	// load config file
+	cfg_filename := "config.yaml"
+	config, err := yaml.ReadFile(cfg_filename)
+	if err != nil {
+		log.Fatalf("readfile(%s): %s", cfg_filename, err)
+	}
 
-    // get english ignore entire string
-    english_ignore, err := config.Get("english_ignore")
-    if err != nil {
-            log.Fatalf("%s parse error: %s\n", english_ignore, err)
-    }
+	// get english ignore entire string
+	english_ignore, err := config.Get("english_ignore")
+	if err != nil {
+		log.Fatalf("%s parse error: %s\n", english_ignore, err)
+	}
 
-    // get each separated words
-    english_ignore_words_list := strings.Fields(english_ignore)
-    for _, word := range english_ignore_words_list {
-            word = strings.TrimSpace(word)
-            english_ignore_words_map[word] = 1
-    }
+	// get each separated words
+	english_ignore_words_list := strings.Fields(english_ignore)
+	for _, word := range english_ignore_words_list {
+		word = strings.TrimSpace(word)
+		english_ignore_words_map[word] = 1
+	}
 }
--- redisbayes_test.go
+++ redisbayes_test.go
@@ -1,25 +1,25 @@
 package redisbayes
 
 import (
-        "testing"
-        "fmt"
+	"fmt"
+	"testing"
 )
 
 func TestTidy(t *testing.T) {
-    test_string := "fjalsdfj $()*#()#*@)&(*&(*^@#*&)!fajs`ldkfj 23"
+	test_string := "fjalsdfj $()*#()#*@)&(*&(*^@#*&)!fajs`ldkfj 23"
 
-    if s_out := Tidy(test_string); s_out != "fjalsdfj fajs ldkfj 23" {
-            t.Errorf("Tidy failed:\n expected: fjalsdfj fajsldkfj 23\n result:%s\n", s_out)
-    }
+	if s_out := Tidy(test_string); s_out != "fjalsdfj fajs ldkfj 23" {
+		t.Errorf("Tidy failed:\n expected: fjalsdfj fajsldkfj 23\n result:%s\n", s_out)
+	}
 }
 
 func TestEnglish_tokenizer(t *testing.T) {
-    test_string := "fjalsdfj $()*#()#*@)&(*&(*^@#*&)!fajs`ldkfj 23"
-    expected_res := []string{"fjalsdfj", "fsjs", "ldkfj", "23"}
+	test_string := "fjalsdfj $()*#()#*@)&(*&(*^@#*&)!fajs`ldkfj 23"
+	expected_res := []string{"fjalsdfj", "fsjs", "ldkfj", "23"}
 
-    words := English_tokenizer(test_string)
+	words := English_tokenizer(test_string)
 
-    if words != expected_res {
-        t.Errorf("tokenizer failed: %s", expected_res)
-    }
+	if words != expected_res {
+		t.Errorf("tokenizer failed: %s", expected_res)
+	}
 }
